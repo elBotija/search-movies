d03f4ae78f64d17aa41e3c685e6ff3d4368fcfcf
murillo <murillo@localhost.localdomain>
2019-01-03 00:57:39 -0300

    Updates

---
+++ precache-manifest.73b5e843316c175239c5c7f2285c6402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "deb1a94bba44e6312e9a",
+    "url": "/search-movies/static/js/runtime~main.deb1a94b.js"
+  },
+  {
+    "revision": "e957183ea9e881ba3f70",
+    "url": "/search-movies/static/js/main.e957183e.chunk.js"
+  },
+  {
+    "revision": "3a272641ae081cdd22ba",
+    "url": "/search-movies/static/js/1.3a272641.chunk.js"
+  },
+  {
+    "revision": "e957183ea9e881ba3f70",
+    "url": "/search-movies/static/css/main.bdf5f8e3.chunk.css"
+  },
+  {
+    "revision": "3a272641ae081cdd22ba",
+    "url": "/search-movies/static/css/1.923068ff.chunk.css"
+  },
+  {
+    "revision": "a15f198b5919a081864cb24a661b7b2b",
+    "url": "/search-movies/index.html"
+  }
+];
